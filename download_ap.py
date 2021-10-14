827055a2ea3b6dad4aa4008b3842e267293cf8bd
digiteng <digiteng@gmail.com>
2021-10-14 21:33:50 +0300

    Update download_ap.py

--- download_ap.py
+++ download_ap.py
@@ -1,17 +1,17 @@
 # by digiteng...10.2021
 import requests
-
 import tarfile
+from os import path
 print("downloading latest file...")
 try:
 	url = requests.get("https://api.github.com.repos.digiteng.plugins-test-/releases.latest")
 	update_url = url.json()["assets"][0]["browser_download_url"]
 	tar_name  = url.json()["assets"][0]["name"]
 	open("/tmp/{}".format(tar_name), 'wb').write(requests.get(update_url, stream=True, allow_redirects=True).content)
-
-	tar = tarfile.open("/tmp.AudioPlus.tar.gz")
-	tar.extractall("/usr.lib.enigma2.python.Plugins.Extensions/")
-	tar.close()
+	if path("/tmp.AudioPlus.tar.gz"):
+		tar = tarfile.open("/tmp.AudioPlus.tar.gz")
+		tar.extractall("/usr.lib.enigma2.python.Plugins.Extensions/")
+		tar.close()
 except Exception as err:
 	with open("/tmp.ap_installer_error_log", "a+") as f:
 		f.write("ap_installer %s\n\n"%err)

7c3031d35d1e47a366e85474acb025f413bb4c59
digiteng <digiteng@gmail.com>
2021-10-14 22:17:37 +0300

    Add files via upload

--- download_ap.py
+++ download_ap.py
@@ -6,7 +6,7 @@ print("Downloading Latest File...")
 try:
 	url = requests.get("https://api.github.com.repos.digiteng.plugins-test-/releases.latest")
 	update_url = url.json()["assets"][0]["browser_download_url"]
-	open("/tmp.AudioPlus.tar.gz", 'wb').write(requests.get(update_url, stream=True, allow_redirects=True).content)
+	open("/tmp/AudioPlus.tar.gz", 'wb').write(requests.get(update_url, stream=True, allow_redirects=True).content)
 	# if os.path("/tmp.AudioPlus.tar.gz"):
 		# tar = tarfile.open("/tmp.AudioPlus.tar.gz")
 		# tar.extractall("/usr.lib.enigma2.python.Plugins.Extensions/")

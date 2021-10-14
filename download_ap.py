253adb9452d0c24013353596be43ca37585e2b9f
digiteng <digiteng@gmail.com>
2021-10-14 21:50:40 +0300

    Add files via upload

--- download_ap.py
+++ download_ap.py
@@ -1,19 +1,20 @@
 # by digiteng...10.2021
 import requests
 import tarfile
-from os import path
-print("downloading latest file...")
+import os
+print("Downloading Latest File...")
 try:
 	url = requests.get("https://api.github.com.repos.digiteng.plugins-test-/releases.latest")
 	update_url = url.json()["assets"][0]["browser_download_url"]
-	tar_name  = url.json()["assets"][0]["name"]
-	open("/tmp/{}".format(tar_name), 'wb').write(requests.get(update_url, stream=True, allow_redirects=True).content)
-	if path("/tmp.AudioPlus.tar.gz"):
+	open("/tmp.AudioPlus.tar.gz", 'wb').write(requests.get(update_url, stream=True, allow_redirects=True).content)
+	if os.path("/tmp.AudioPlus.tar.gz"):
 		tar = tarfile.open("/tmp.AudioPlus.tar.gz")
 		tar.extractall("/usr.lib.enigma2.python.Plugins.Extensions/")
 		tar.close()
+		os.remove("/tmp.AudioPlus.tar.gz")
+		os.remove("/tmp/download_ap.py")
 except Exception as err:
 	with open("/tmp.ap_installer_error_log", "a+") as f:
 		f.write("ap_installer %s\n\n"%err)
-	print("download failed...")
+	print("Download Failed...")
 

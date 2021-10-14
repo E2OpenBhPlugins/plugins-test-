bc7b2f3117b6ff3627e6086248d15c6df61038d7
digiteng <digiteng@gmail.com>
2021-10-14 22:27:32 +0300

    Add files via upload

--- ap_intaller.sh
+++ ap_intaller.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
+# wget -q "--no-check-certificate" https://github.com/digiteng/plugins-test-/raw/master/ap_intaller.sh -O - | /bin/sh
 sleep 1
 echo "Old Version Deleting..."
-sleep 1
-# if [ -d /usr/lib/enigma2/python/Plugins/Extensions/AudioPlus ]; then
 rm -rf /usr/lib/enigma2/python/Plugins/Extensions/AudioPlus
-# fi
 rm -rf /tmp/AudioPlus.tar.gz
 rm -rf /tmp/download_ap.py
- 
 wget "https://github.com/digiteng/plugins-test-/raw/master/download_ap.py" -P /tmp/
 sleep 2
 if [ -f /tmp/download_ap.py ]; then
@@ -16,12 +13,11 @@ fi
 sleep 1
 if [ -f /tmp/AudioPlus.tar.gz ]; then
 	tar -xzf /tmp/AudioPlus.tar.gz -C /usr/lib/enigma2/python/Plugins/Extensions
-
 fi
 echo "New Version Installed"
-# sleep 2
-# init 4
-# echo "Restarting your enigma2 gui..."
-# init 3
+sleep 2
+init 4
+echo "Restarting your enigma2 gui..."
+init 3
 exit 0
 
